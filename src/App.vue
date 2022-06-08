7b865aebba4d3f54b4e0790460781a90585c9382
Sofus Lund <sofuslund.dev@gmail.com>
2022-06-08 21:32:40 +0200

    Add colors

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 <template>
     <div class="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="#">Sofus</a>.
     </div>
 </template>
 
