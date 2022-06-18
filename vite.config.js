caa87d732dbd478dd2d3230f9d4a503d34daae21
Sofus Lund <sofuslund.dev@gmail.com>
2022-06-18 13:34:02 +0200

    Add @vitejs/plugin-legacy to support older browsers

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    legacy({}),
+    vue()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
